ff97b5e0d8bad61fc229d127403610aa223b45d1
MarmoW <115847012+MarmoW@users.noreply.github.com>
2023-03-02 22:52:22 -0300

    test

--- src/controllers/UrlController.js
+++ src/controllers/UrlController.js
@@ -70,7 +70,7 @@ export async function OpenShortUrl(req, res){
 
         console.log(fullUrl.rows[0].visitCount)
 
-        res.redirect(fullUrl.rows[0].url)
+        res.redirect(fullUrl.url)
 
     }catch(err){
         res.status(500).send(err.message)
